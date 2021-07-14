a7be23e41326427901ddd002f3928430f345df2c
Lucas Juste <lu.scatollini@hotmail.com>
2021-07-14 15:18:11 -0300

    first commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,29 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) { }
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (user.admin == false) {
+      throw new Error('User is not admin')
+    }
+
+    if (!user) {
+      throw new Error('Need sign up for continue')
+    }
+
+    const users = this.usersRepository.list()
+    return users
+
+
+  }
+}
+
+export { ListAllUsersUseCase };
